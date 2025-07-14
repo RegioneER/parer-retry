7477301f47d3dd13b6c0bb15cb2359a58bc9c0bc
parerworker <DevPARER@regione.emilia-romagna.it>
2025-07-14 11:25:11 +0200

    Update project to latest tag parer-retry-2.3.0

--- src/main/java/it/eng/parer/retry/ParerRetryConfigurationBuilder.java
+++ src/main/java/it/eng/parer/retry/ParerRetryConfigurationBuilder.java
@@ -1,18 +1,14 @@
 /*
  * Engineering Ingegneria Informatica S.p.A.
  *
- * Copyright (C) 2023 Regione Emilia-Romagna
- * <p/>
- * This program is free software: you can redistribute it and/or modify it under the terms of
- * the GNU Affero General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
- * <p/>
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Affero General Public License for more details.
- * <p/>
- * You should have received a copy of the GNU Affero General Public License along with this program.
- * If not, see <https://www.gnu.org/licenses/>.
+ * Copyright (C) 2023 Regione Emilia-Romagna <p/> This program is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License, or (at your option)
+ * any later version. <p/> This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. <p/> You should
+ * have received a copy of the GNU Affero General Public License along with this program. If not,
+ * see <https://www.gnu.org/licenses/>.
  */
 
 package it.eng.parer.retry;
@@ -46,76 +42,71 @@ public class ParerRetryConfigurationBuilder {
     /**
      * Aggiunge una {@link TimeoutRetryPolicy}. Il valore del timeout è espresso in ms.
      *
-     * @param timeout
-     *            in ms
+     * @param timeout in ms
      *
      * @return {@link ParerRetryConfigurationBuilder}
      */
     public ParerRetryConfigurationBuilder withTimeout(long timeout) {
-        TimeoutRetryPolicy policy = new TimeoutRetryPolicy();
-        policy.setTimeout(timeout);
-        this.policies.add(policy);
+	TimeoutRetryPolicy policy = new TimeoutRetryPolicy();
+	policy.setTimeout(timeout);
+	this.policies.add(policy);
 
-        return this;
+	return this;
     }
 
     /**
      * Aggiunge un {@link SimpleRetryPolicy}.
      *
-     * @param maxAttemps
-     *            numero massimo di tentativi
+     * @param maxAttemps numero massimo di tentativi
      *
      * @return {@link ParerRetryConfigurationBuilder}
      */
     public ParerRetryConfigurationBuilder withMaxAttemps(int maxAttemps) {
-        SimpleRetryPolicy policy = new SimpleRetryPolicy();
-        policy.setMaxAttempts(maxAttemps);
-        this.policies.add(policy);
-        return this;
+	SimpleRetryPolicy policy = new SimpleRetryPolicy();
+	policy.setMaxAttempts(maxAttemps);
+	this.policies.add(policy);
+	return this;
     }
 
     /**
      * Imposta un periodo di backoff tra una chiamata e l'altra.
      *
-     * @param backOffPeriod
-     *            espresso in ms
+     * @param backOffPeriod espresso in ms
      *
      * @return {@link ParerRetryConfigurationBuilder}
      */
     public ParerRetryConfigurationBuilder withBackoffPeriod(long backOffPeriod) {
-        FixedBackOffPolicy policy = new FixedBackOffPolicy();
-        policy.setBackOffPeriod(backOffPeriod);
-        this.backOffPolicy = policy;
-        return this;
+	FixedBackOffPolicy policy = new FixedBackOffPolicy();
+	policy.setBackOffPeriod(backOffPeriod);
+	this.backOffPolicy = policy;
+	return this;
     }
 
     /**
      * Imposta le configurazioni relative ad un'approccio a rottura di circuito.
      *
-     * @param openTimeout
-     *            timeout di apertura
-     * @param resetTimeout
-     *            timeout di reset
+     * @param openTimeout  timeout di apertura
+     * @param resetTimeout timeout di reset
      *
      * @return {@link ParerRetryConfigurationBuilder}
      */
     public ParerRetryConfigurationBuilder withCircuitBreaker(long openTimeout, long resetTimeout) {
-        this.cbOpenTimeout = openTimeout;
-        this.cbResetTimeout = resetTimeout;
-        return this;
+	this.cbOpenTimeout = openTimeout;
+	this.cbResetTimeout = resetTimeout;
+	return this;
     }
 
     /**
-     * Imposta le configurazioni relative all'approccio ottimistico o pessimistico per le policy composite.
+     * Imposta le configurazioni relative all'approccio ottimistico o pessimistico per le policy
+     * composite.
      *
-     * @param compositePolicy
-     *            valore da assegnare alla logica di valutazione delle policy composite
+     * @param compositePolicy valore da assegnare alla logica di valutazione delle policy composite
      *
      * @return {@link ParerRetryConfigurationBuilder}
      */
     public ParerRetryConfigurationBuilder withOptimisticCompositePolicy(boolean compositePolicy) {
-        this.optimisticCompositePolicy = compositePolicy;
-        return this;
+	this.optimisticCompositePolicy = compositePolicy;
+	return this;
     }
 
     /**
@@ -125,33 +116,33 @@ public class ParerRetryConfigurationBuilder {
      */
     public ParerRetryConfiguration build() {
 
-        ParerRetryConfiguration restClient = new ParerRetryConfiguration();
-        RetryTemplate retryTemplate = new RetryTemplate();
+	ParerRetryConfiguration restClient = new ParerRetryConfiguration();
+	RetryTemplate retryTemplate = new RetryTemplate();
 
-        if (this.backOffPolicy != null) {
-            retryTemplate.setBackOffPolicy(this.backOffPolicy);
-        }
+	if (this.backOffPolicy != null) {
+	    retryTemplate.setBackOffPolicy(this.backOffPolicy);
+	}
 
-        // predefinito, 10 tentativi.
-        RetryPolicy policy = new SimpleRetryPolicy(DEFAULT_MAX_RETRY);
-        if (!policies.isEmpty()) {
-            CompositeRetryPolicy compositePolicy = new CompositeRetryPolicy();
-            compositePolicy.setPolicies(policies.toArray(new RetryPolicy[] {}));
-            compositePolicy.setOptimistic(this.optimisticCompositePolicy);
+	// predefinito, 10 tentativi.
+	RetryPolicy policy = new SimpleRetryPolicy(DEFAULT_MAX_RETRY);
+	if (!policies.isEmpty()) {
+	    CompositeRetryPolicy compositePolicy = new CompositeRetryPolicy();
+	    compositePolicy.setPolicies(policies.toArray(new RetryPolicy[] {}));
+	    compositePolicy.setOptimistic(this.optimisticCompositePolicy);
 
-            policy = compositePolicy;
-        }
+	    policy = compositePolicy;
+	}
 
-        if (cbOpenTimeout > 0 && cbResetTimeout > 0) {
-            CircuitBreakerRetryPolicy circuitBreakerPolicy = new CircuitBreakerRetryPolicy(policy);
-            circuitBreakerPolicy.setOpenTimeout(cbOpenTimeout);
-            circuitBreakerPolicy.setResetTimeout(cbResetTimeout);
-            policy = circuitBreakerPolicy;
-        }
-        retryTemplate.setRetryPolicy(policy);
+	if (cbOpenTimeout > 0 && cbResetTimeout > 0) {
+	    CircuitBreakerRetryPolicy circuitBreakerPolicy = new CircuitBreakerRetryPolicy(policy);
+	    circuitBreakerPolicy.setOpenTimeout(cbOpenTimeout);
+	    circuitBreakerPolicy.setResetTimeout(cbResetTimeout);
+	    policy = circuitBreakerPolicy;
+	}
+	retryTemplate.setRetryPolicy(policy);
 
-        restClient.setRetryTemplate(retryTemplate);
+	restClient.setRetryTemplate(retryTemplate);
 
-        return restClient;
+	return restClient;
     }
 }
--- src/test/java/it/eng/parer/retry/TestRetry.java
+++ src/test/java/it/eng/parer/retry/TestRetry.java
@@ -1,18 +1,14 @@
 /*
  * Engineering Ingegneria Informatica S.p.A.
  *
- * Copyright (C) 2023 Regione Emilia-Romagna
- * <p/>
- * This program is free software: you can redistribute it and/or modify it under the terms of
- * the GNU Affero General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
- * <p/>
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Affero General Public License for more details.
- * <p/>
- * You should have received a copy of the GNU Affero General Public License along with this program.
- * If not, see <https://www.gnu.org/licenses/>.
+ * Copyright (C) 2023 Regione Emilia-Romagna <p/> This program is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License, or (at your option)
+ * any later version. <p/> This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. <p/> You should
+ * have received a copy of the GNU Affero General Public License along with this program. If not,
+ * see <https://www.gnu.org/licenses/>.
  */
 
 package it.eng.parer.retry;
@@ -24,18 +20,21 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClientException;
@@ -46,15 +45,25 @@ import org.springframework.web.client.RestTemplate;
  *
  * @author Snidero_L
  */
-public class TestRetry {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {
+	TestConfig.class })
+class TestRetry {
 
     private final Logger log = LoggerFactory.getLogger(TestRetry.class);
 
     private static final int TIMEOUT = 10000;
 
-    private static final String CRYPTO_LOCALE = "http://localhost:8091/";
-    private static final String CRYPTO_SVIL_OKD = "https://verificafirma-crypto-latest.apps.test-parerocp.ente.regione.emr.it/";
-    private static final String CRYPTO_TEST_OKD = "https://verificafirma-crypto-latest.apps.test-parerocp.ente.regione.emr.it/";
+    @Value("${crypto.snap.endpoint}")
+    private String cryptoSnapEndpoint;
+
+    @Value("${crypto.test.endpoint}")
+    private String cryptoTestEndpoint;
+
+    @Value("${crypto.local.endpoint}")
+    private String cryptoLocalEndpoint;
+
     private static final List<URI> BAD_ENDPOINTS = new ArrayList<>();
 
     private RestTemplate restTemplate;
@@ -62,200 +71,199 @@ public class TestRetry {
     private String preferredEndpoint;
 
     @BeforeAll
-    public static void setUpClass() {
-        BAD_ENDPOINTS.add(URI.create(CRYPTO_LOCALE));
-        BAD_ENDPOINTS.add(URI.create("Br0kenUr1"));
-        BAD_ENDPOINTS.add(URI.create("http://localhost:8092/"));
-        BAD_ENDPOINTS.add(URI.create("http://localhost:8093/"));
-        BAD_ENDPOINTS.add(URI.create("//////////"));
-        BAD_ENDPOINTS.add(URI.create("../../"));
-    }
-
-    @AfterAll
-    public static void tearDownClass() {
+    void setUpClass() {
+	BAD_ENDPOINTS.add(URI.create(cryptoLocalEndpoint));
+	BAD_ENDPOINTS.add(URI.create("Br0kenUr1"));
+	BAD_ENDPOINTS.add(URI.create("http://localhost:8092/"));
+	BAD_ENDPOINTS.add(URI.create("http://localhost:8093/"));
+	BAD_ENDPOINTS.add(URI.create("//////////"));
+	BAD_ENDPOINTS.add(URI.create("../../"));
     }
 
     @BeforeEach
-    public void setUp() {
-        restTemplate = new RestTemplate();
+    void setUp() {
+	restTemplate = new RestTemplate();
 
-        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
-        clientHttpRequestFactory.setReadTimeout(TIMEOUT);
-        clientHttpRequestFactory.setConnectTimeout(TIMEOUT);
+	HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+	clientHttpRequestFactory.setReadTimeout(TIMEOUT);
+	clientHttpRequestFactory.setConnectTimeout(TIMEOUT);
 
-        restTemplate.setRequestFactory(clientHttpRequestFactory);
+	restTemplate.setRequestFactory(clientHttpRequestFactory);
 
-        preferredEndpoint = CRYPTO_TEST_OKD;
-    }
-
-    @AfterEach
-    public void tearDown() {
+	preferredEndpoint = cryptoTestEndpoint;
     }
 
     @Test
     void testTST() {
-        log.info("Test configurazione predefinita");
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = buildValidRequestEntity();
+	log.info("Test configurazione predefinita");
+	HttpEntity<MultiValueMap<String, Object>> requestEntity = buildValidRequestEntity();
 
-        String endpoint = preferredEndpoint + "api/tst";
+	String endpoint = preferredEndpoint + "api/tst";
 
-        ParerRetryConfiguration retryClient = ParerRetryConfiguration.defaultInstance();
-        restTemplate.getInterceptors().removeIf(i -> true);
-        restTemplate.getInterceptors().add(new RestRetryInterceptor(BAD_ENDPOINTS, retryClient));
+	ParerRetryConfiguration retryClient = ParerRetryConfiguration.defaultInstance();
+	restTemplate.getInterceptors().removeIf(i -> true);
+	restTemplate.getInterceptors().add(new RestRetryInterceptor(BAD_ENDPOINTS, retryClient));
 
-        String parerTST = restTemplate.postForObject(endpoint, requestEntity, String.class);
-        assertNotNull(parerTST);
+	String parerTST = restTemplate.postForObject(endpoint, requestEntity, String.class);
+	assertNotNull(parerTST);
     }
 
     @Test
     void testTSTWithNoValidURL() {
 
-        log.info("Test senza URL raggiungibili");
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = buildValidRequestEntity();
+	log.info("Test senza URL raggiungibili");
+	HttpEntity<MultiValueMap<String, Object>> requestEntity = buildValidRequestEntity();
 
-        String endpoint = "http://localhost:8090/api/tst";
+	String endpoint = "http://localhost:8090/api/tst";
 
-        ParerRetryConfiguration retryClient = ParerRetryConfiguration.defaultInstance();
-        restTemplate.getInterceptors().removeIf(i -> true);
-        restTemplate.getInterceptors().add(new RestRetryInterceptor(BAD_ENDPOINTS, retryClient));
+	ParerRetryConfiguration retryClient = ParerRetryConfiguration.defaultInstance();
+	restTemplate.getInterceptors().removeIf(i -> true);
+	restTemplate.getInterceptors().add(new RestRetryInterceptor(BAD_ENDPOINTS, retryClient));
 
-        assertThrows(RestClientException.class,
-                () -> restTemplate.postForObject(endpoint, requestEntity, Object.class));
+	assertThrows(RestClientException.class,
+		() -> restTemplate.postForObject(endpoint, requestEntity, Object.class));
 
     }
 
     @Test
     void testTSTWithSomeBadURI() {
 
-        log.info("Test con alcune URL raggiungibili");
+	log.info("Test con alcune URL raggiungibili");
 
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = buildValidRequestEntity();
+	HttpEntity<MultiValueMap<String, Object>> requestEntity = buildValidRequestEntity();
 
-        String endpoint = "http://localhost:8090/api/tst";
+	String endpoint = "http://localhost:8090/api/tst";
 
-        // Endpoint errati
-        List<URI> endPoints = new ArrayList<>(BAD_ENDPOINTS);
-        // Aggiungo endpoint "buono
-        endPoints.add(URI.create(CRYPTO_SVIL_OKD));
+	// Endpoint errati
+	List<URI> endPoints = new ArrayList<>(BAD_ENDPOINTS);
+	// Aggiungo endpoint "buono
+	endPoints.add(URI.create(cryptoSnapEndpoint));
 
-        ParerRetryConfiguration retryClient = ParerRetryConfiguration.defaultInstance();
-        restTemplate.getInterceptors().removeIf(i -> true);
-        restTemplate.getInterceptors().add(new RestRetryInterceptor(endPoints, retryClient));
+	ParerRetryConfiguration retryClient = ParerRetryConfiguration.defaultInstance();
+	restTemplate.getInterceptors().removeIf(i -> true);
+	restTemplate.getInterceptors().add(new RestRetryInterceptor(endPoints, retryClient));
 
-        String parerTST = restTemplate.postForObject(endpoint, requestEntity, String.class);
-        assertNotNull(parerTST);
+	String parerTST = restTemplate.postForObject(endpoint, requestEntity, String.class);
+	assertNotNull(parerTST);
 
     }
 
     @Test
     void testTSTWitoutMandatoryParameter() {
 
-        log.info("Test senza parametro obbligatorio");
+	log.info("Test senza parametro obbligatorio");
 
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = buildInvalidRequestEntity();
+	HttpEntity<MultiValueMap<String, Object>> requestEntity = buildInvalidRequestEntity();
 
-        String endpoint = "http://localhost:8090/api/tst";
+	String endpoint = "http://localhost:8090/api/tst";
 
-        // Endpoint errati
-        List<URI> endPoints = new ArrayList<>(BAD_ENDPOINTS);
-        // Aggiungo endpoint "buono
-        endPoints.add(URI.create(CRYPTO_SVIL_OKD));
+	// Endpoint errati
+	List<URI> endPoints = new ArrayList<>(BAD_ENDPOINTS);
+	// Aggiungo endpoint "buono
+	endPoints.add(URI.create(cryptoSnapEndpoint));
 
-        ParerRetryConfiguration retryClient = ParerRetryConfiguration.defaultInstance();
+	ParerRetryConfiguration retryClient = ParerRetryConfiguration.defaultInstance();
 
-        restTemplate.getInterceptors().removeIf(i -> true);
-        restTemplate.getInterceptors().add(new RestRetryInterceptor(endPoints, retryClient));
+	restTemplate.getInterceptors().removeIf(i -> true);
+	restTemplate.getInterceptors().add(new RestRetryInterceptor(endPoints, retryClient));
 
-        assertThrows(Exception.class, () -> restTemplate.postForObject(endpoint, requestEntity, String.class));
+	assertThrows(Exception.class,
+		() -> restTemplate.postForObject(endpoint, requestEntity, String.class));
     }
 
     /**
-     * Test policy composita con logica "ottimistica": timeout molto piccolo e molte retry. Effettuerà tutte le retry.
+     * Test policy composita con logica "ottimistica": timeout molto piccolo e molte retry.
+     * Effettuerà tutte le retry.
      *
      */
     @Test
     void testOptimisticParameter() {
 
-        log.info("Test con policy composita e logica ottimistica");
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = buildValidRequestEntity();
+	log.info("Test con policy composita e logica ottimistica");
+	HttpEntity<MultiValueMap<String, Object>> requestEntity = buildValidRequestEntity();
 
-        String endpoint = "http://localhost:8090/api/tst";
+	String endpoint = "http://localhost:8090/api/tst";
 
-        // Endpoint errati
-        List<URI> endPoints = new ArrayList<>(BAD_ENDPOINTS);
-        // Aggiungo endpoint "buono
-        endPoints.add(URI.create(CRYPTO_SVIL_OKD));
+	// Endpoint errati
+	List<URI> endPoints = new ArrayList<>(BAD_ENDPOINTS);
+	// Aggiungo endpoint "buono
+	endPoints.add(URI.create(cryptoSnapEndpoint));
 
-        ParerRetryConfiguration retryClientConfiguration = new ParerRetryConfigurationBuilder().withMaxAttemps(10)
-                .withOptimisticCompositePolicy(true).withTimeout(1L).build();
+	ParerRetryConfiguration retryClientConfiguration = new ParerRetryConfigurationBuilder()
+		.withMaxAttemps(10).withOptimisticCompositePolicy(true).withTimeout(1L).build();
 
-        restTemplate.getInterceptors().removeIf(i -> true);
-        restTemplate.getInterceptors().add(new RestRetryInterceptor(endPoints, retryClientConfiguration));
+	restTemplate.getInterceptors().removeIf(i -> true);
+	restTemplate.getInterceptors()
+		.add(new RestRetryInterceptor(endPoints, retryClientConfiguration));
 
-        String parerTST = restTemplate.postForObject(endpoint, requestEntity, String.class);
-        assertNotNull(parerTST);
+	String parerTST = restTemplate.postForObject(endpoint, requestEntity, String.class);
+	assertNotNull(parerTST);
     }
 
     /**
-     * Test policy composita con logica "pessimistica": timeout molto piccolo e molte retry. Effettuerà solo la prima.
+     * Test policy composita con logica "pessimistica": timeout molto piccolo e molte retry.
+     * Effettuerà solo la prima.
      *
      */
     @Test
     void testPessimisticParameter() {
-        log.info("Test con policy composita e logica pessimistica");
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = buildValidRequestEntity();
+	log.info("Test con policy composita e logica pessimistica");
+	HttpEntity<MultiValueMap<String, Object>> requestEntity = buildValidRequestEntity();
 
-        String endpoint = "http://localhost:8090/api/tst";
+	String endpoint = "http://localhost:8090/api/tst";
 
-        // Endpoint errati
-        List<URI> endPoints = new ArrayList<>(BAD_ENDPOINTS);
-        // Aggiungo endpoint "buono
-        endPoints.add(URI.create(CRYPTO_SVIL_OKD));
+	// Endpoint errati
+	List<URI> endPoints = new ArrayList<>(BAD_ENDPOINTS);
+	// Aggiungo endpoint "buono
+	endPoints.add(URI.create(cryptoSnapEndpoint));
 
-        ParerRetryConfiguration retryClientConfiguration = new ParerRetryConfigurationBuilder().withMaxAttemps(10)
-                .withOptimisticCompositePolicy(false).withTimeout(1L).build();
+	ParerRetryConfiguration retryClientConfiguration = new ParerRetryConfigurationBuilder()
+		.withMaxAttemps(10).withOptimisticCompositePolicy(false).withTimeout(1L).build();
 
-        restTemplate.getInterceptors().removeIf(i -> true);
-        restTemplate.getInterceptors().add(new RestRetryInterceptor(endPoints, retryClientConfiguration));
+	restTemplate.getInterceptors().removeIf(i -> true);
+	restTemplate.getInterceptors()
+		.add(new RestRetryInterceptor(endPoints, retryClientConfiguration));
 
-        assertThrows(RestClientException.class,
-                () -> restTemplate.postForObject(endpoint, requestEntity, String.class));
+	assertThrows(RestClientException.class,
+		() -> restTemplate.postForObject(endpoint, requestEntity, String.class));
     }
 
     private HttpEntity<MultiValueMap<String, Object>> buildValidRequestEntity() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+	HttpHeaders headers = new HttpHeaders();
+	headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 
-        org.springframework.core.io.Resource resource = new ByteArrayResource("Ceci n'est pas un test".getBytes()) {
-            @Override
-            public String getFilename() {
-                return "requestTst";
-            }
-        };
+	org.springframework.core.io.Resource resource = new ByteArrayResource(
+		"Ceci n'est pas un test".getBytes()) {
+	    @Override
+	    public String getFilename() {
+		return "requestTst";
+	    }
+	};
 
-        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-        body.add("description", "Richiesta TST");
-        body.add("file", resource);
+	MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+	body.add("description", "Richiesta TST");
+	body.add("file", resource);
 
-        return new HttpEntity<>(body, headers);
+	return new HttpEntity<>(body, headers);
     }
 
     private HttpEntity<MultiValueMap<String, Object>> buildInvalidRequestEntity() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+	HttpHeaders headers = new HttpHeaders();
+	headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 
-        org.springframework.core.io.Resource resource = new ByteArrayResource("Ceci n'est pas un test".getBytes()) {
-            @Override
-            public String getFilename() {
-                return "requestTst";
-            }
-        };
+	org.springframework.core.io.Resource resource = new ByteArrayResource(
+		"Ceci n'est pas un test".getBytes()) {
+	    @Override
+	    public String getFilename() {
+		return "requestTst";
+	    }
+	};
 
-        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-        // Tolta la descrizione che è obbligatoria
-        body.add("file", resource);
+	MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+	// Tolta la descrizione che è obbligatoria
+	body.add("file", resource);
 
-        return new HttpEntity<>(body, headers);
+	return new HttpEntity<>(body, headers);
     }
 
 }
